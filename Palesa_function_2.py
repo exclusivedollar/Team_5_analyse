a0cf1fee7a4b6c93566af88a38146b4b966dd8f2
PTStace <68692872+PTStace@users.noreply.github.com>
2020-07-31 10:58:59 +0200

    for loop

--- Palesa_function_2.py
+++ Palesa_function_2.py
@@ -1,2 +1,11 @@
 def five_num_summary (items):
+    for items in gauteng:
+        items = np.quantile(gauteng,[1, .50, 0, .25, .75])
+
+        max = round(items[0],2)
+        median = round(items[1],2)
+        min = round(items[2],2)
+        q1
+
+
     return
