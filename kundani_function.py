d2ec3ebce0dcd042dd7908eb58815187d63ebacb
K-Netshiongolwe <netshiongolwekundani@gmail.com>
2020-08-04 00:41:31 -0700

    Update kundani_function.py

--- kundani_function.py
+++ kundani_function.py
@@ -1,12 +1,13 @@
 def dictionary_of_metrics(items):
 
-    dict ={'mean':round(np.mean(items),2),
-         'median':round(np.median(items),2),
-         'var':round(np.var(items,ddof=1),2),
-         'std':round(np.mean(items,ddof=1),2),
-         'max':round(np.max(items),2)}
+    dict = {'mean':round(np.mean(items),2),
+          'median':round(np.median(items),2),
+          'var':round(np.var(items,ddof=1),2),
+          'std':round(np.mean(items,ddof=1),2),
+          'max':round(np.max(items),2)}
 
 
 
      return dict
+     
 gauteng = ebp_df['Gauteng']
