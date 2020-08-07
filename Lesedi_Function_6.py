dfbdaecf9d523d53932f97fa965135ee489970ab
Lesedi-Coder <59532191+Lesedi-Coder@users.noreply.github.com>
2020-08-07 04:15:03 +0200

    Update Lesedi_Function_6.py

--- Lesedi_Function_6.py
+++ Lesedi_Function_6.py
@@ -1,5 +1,4 @@
 def word_splitter(df):    
-    
     dframe = pd.DataFrame(df,columns =['Tweets','Date','Split Tweets'])
     dframe['Date'] = dates
     dframe['Split Tweets']= dframe['Tweets'].str.lower()
