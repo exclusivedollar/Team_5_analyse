97ce485130c6960389b2b75a1a43f768b7f1aa7b
Grewies <68637353+Grewies@users.noreply.github.com>
2020-08-11 22:08:03 +0200

    Update Chuene_Function_3.py

--- Chuene_Function_3.py
+++ Chuene_Function_3.py
@@ -1,20 +1,21 @@
 ### START FUNCTION
-""" This function takes as input a list of these datetime strings, each string formatted as 'yyyy-mm-dd hh:mm:ss' 
+"""This function takes as input a list of these datetime strings,
+each string formatted as 'yyyy-mm-dd hh:mm:ss'
 and returns only the date in 'yyyy-mm-dd' format.
-
-    Parameters:
-    -----------
-    items: (list), list of datetime strings.
-
+    input:
+    list of datetime strings as 'yyyy-mm-dd hh:mm:ss'
     Returns:
-    --------
-returns a list of strings where each element in the returned list contains only the date in the 'yyyy-mm-dd' format."""
+returns a list of strings where each element in
+the returned list contains only the date in the 'yyyy-mm-dd' format."""
 
-def date_parser(dates):                       # The function date_parser takes in a datetime string input formatted as 'yyyy-mm-dd hh:mm:ss'
-    date_only =[]                             # An empty date list (to add sublists in it in the for loop)
-    for date in dates:                        # Setting up the condition
-        my_list = date[: len(date) - 9]       # 
-        date_only.append(my_list)             # Creating a sublist from my_list
-        date_only[:]                          #
-    return date_only                          # Return the date only in 'yyyy-mm-dd' format
+def date_parser(dates):
+    # Creating a new date list.
+    date_only = []
+    for date in dates:
+        my_list = date[:len(date)-9]
+# Append values from my list to date_only list.
+        date_only.append(my_list)
+        date_only[:]
+# returned list contains only the date in the 'yyyy-mm-dd' format.
+    return date_only
 ### END FUNCTION
