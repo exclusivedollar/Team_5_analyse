f6b49c7f4a2780bc8fa33f8a1a61cdf22a53be6c
Lesedi-Coder <59532191+Lesedi-Coder@users.noreply.github.com>
2020-08-12 03:21:32 +0200

    Inline Comments Added

--- Lesedi_Function_6.py
+++ Lesedi_Function_6.py
@@ -5,9 +5,12 @@ def word_splitter(df):
        
        Arguments : Takes in a pandas dataframe as an input
        Returns: returns a modified dataframe""" 
-       
-    dframe = pd.DataFrame(df,columns =['Tweets','Date','Split Tweets'])
+   #Creates a new Dataframe using the given one
+    dframe = pd.DataFrame(df,columns =['Tweets','Date','Split Tweets'])   
+    #Creates a column called 'Date' and assign given dates to it         
     dframe['Date'] = dates
+    #Modifies the 'Split Tweets' column by making every character lowercase and splits
     dframe['Split Tweets']= dframe['Tweets'].str.lower()
-    dframe['Split Tweets']= dframe['Split Tweets'].str.split()  
+    dframe['Split Tweets']= dframe['Split Tweets'].str.split()
+    #Returns the modified Dataframe  
     return dframe
