529fe3b06205b18d171522a4c2b35ad49cc5a714
exclusivedollar <31101097+exclusivedollar@users.noreply.github.com>
2020-08-12 11:54:57 +0200

    Update Sammy_Function_5.py
    Added Docstring

--- Sammy_Function_5.py
+++ Sammy_Function_5.py
@@ -1,4 +1,6 @@
 def number_of_tweets_per_day(df):
+    """ The Function takes in a pandas dataframe as input.
+        It then returns a new dataframe, grouped by day, with the number of tweets for that day. """
     
     df["Date"] = pd.to_datetime(df["Date"]).dt.date
     df1 = df.groupby('Date').count()
