1da34f43461aa643833dc5ca796d529a2446ca70
exclusivedollar <31101097+exclusivedollar@users.noreply.github.com>
2020-08-12 18:51:57 +0200

    Update Palesa_Function_2.py
    Fixed indentation errors

--- Palesa_Function_2.py
+++ Palesa_Function_2.py
@@ -1,8 +1,9 @@
 def five_num_summary (items):
+    
     for item in items:
         items = np.quantile(items,[1, .50, 0, .25, .75])
         
-"""" returns a five number summary from a list of items"""
+        """" returns a five number summary from a list of items"""
         max = round(items[0],2) # all numerical values should be rounded to two decimal places
         median = round(items[1],2)
         min = round(items[2],2)
