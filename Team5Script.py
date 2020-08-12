749c8143c20ccbf0e5de2525ebea93616f0ccdad
PTStace <68692872+PTStace@users.noreply.github.com>
2020-08-12 23:48:15 +0200

    Update Team5Script.py

--- Team5Script.py
+++ Team5Script.py
@@ -74,7 +74,16 @@ def extract_municipality_hashtags(df):
     new_df['hashtags'] = final_hash
     
     #returns the modified dataframe.
-    return new_df 
+    return new_df
+
+def number_of_tweets_per_day(df):
+    """ The Function takes in a pandas dataframe as input.
+        It then returns a new dataframe, grouped by day,
+        with the number of tweets for that day. """
+    df["Date"] = pd.to_datetime(df["Date"]).dt.date
+    df1 = df.groupby('Date').count()
+    return df1
+
     
 def word_splitter(df):    
     dframe = pd.DataFrame(df,columns =['Tweets','Date','Split Tweets'])
@@ -83,53 +92,15 @@ def word_splitter(df):
     dframe['Split Tweets']= dframe['Split Tweets'].str.split()  
     return dframe
     
-def stop_words_remover(df): #takes in a pandas dataframe as input
-    # your code here
-    """function to remove english stop words"""
-
-    new_df = pd.DataFrame([]) #creat new dataframe
-    tokenised_list = [] # tokenised sentence
-    stop_words_dict = {'stopwords':[
-        'where', 'done', 'if', 'before', 'll', 'very', 'keep', 'something', 'nothing', 'thereupon', 
-        'may', 'why', 'â€™s', 'therefore', 'you', 'with', 'towards', 'make', 'really', 'few', 'former', 
-        'during', 'mine', 'do', 'would', 'of', 'off', 'six', 'yourself', 'becoming', 'through', 
-        'seeming', 'hence', 'us', 'anywhere', 'regarding', 'whole', 'down', 'seem', 'whereas', 'to', 
-        'their', 'various', 'thereafter', 'â€˜d', 'above', 'put', 'sometime', 'moreover', 'whoever', 'although', 
-        'at', 'four', 'each', 'among', 'whatever', 'any', 'anyhow', 'herein', 'become', 'last', 'between', 'still', 
-        'was', 'almost', 'twelve', 'used', 'who', 'go', 'not', 'enough', 'well', 'â€™ve', 'might', 'see', 'whose', 
-        'everywhere', 'yourselves', 'across', 'myself', 'further', 'did', 'then', 'is', 'except', 'up', 'take', 
-        'became', 'however', 'many', 'thence', 'onto', 'â€˜m', 'my', 'own', 'must', 'wherein', 'elsewhere', 'behind', 
-        'becomes', 'alone', 'due', 'being', 'neither', 'a', 'over', 'beside', 'fifteen', 'meanwhile', 'upon', 'next', 
-        'forty', 'what', 'less', 'and', 'please', 'toward', 'about', 'below', 'hereafter', 'whether', 'yet', 'nor', 
-        'against', 'whereupon', 'top', 'first', 'three', 'show', 'per', 'five', 'two', 'ourselves', 'whenever', 
-        'get', 'thereby', 'noone', 'had', 'now', 'everyone', 'everything', 'nowhere', 'ca', 'though', 'least', 
-        'so', 'both', 'otherwise', 'whereby', 'unless', 'somewhere', 'give', 'formerly', 'â€™d', 'under', 
-        'while', 'empty', 'doing', 'besides', 'thus', 'this', 'anyone', 'its', 'after', 'bottom', 'call', 
-        'nâ€™t', 'name', 'even', 'eleven', 'by', 'from', 'when', 'or', 'anyway', 'how', 'the', 'all', 
-        'much', 'another', 'since', 'hundred', 'serious', 'â€˜ve', 'ever', 'out', 'full', 'themselves', 
-        'been', 'in', "'d", 'wherever', 'part', 'someone', 'therein', 'can', 'seemed', 'hereby', 'others', 
-        "'s", "'re", 'most', 'one', "n't", 'into', 'some', 'will', 'these', 'twenty', 'here', 'as', 'nobody', 
-        'also', 'along', 'than', 'anything', 'he', 'there', 'does', 'we', 'â€™ll', 'latterly', 'are', 'ten', 
-        'hers', 'should', 'they', 'â€˜s', 'either', 'am', 'be', 'perhaps', 'â€™re', 'only', 'namely', 'sixty', 
-        'made', "'m", 'always', 'those', 'have', 'again', 'her', 'once', 'ours', 'herself', 'else', 'has', 'nine', 
-        'more', 'sometimes', 'your', 'yours', 'that', 'around', 'his', 'indeed', 'mostly', 'cannot', 'â€˜ll', 'too', 
-        'seems', 'â€™m', 'himself', 'latter', 'whither', 'amount', 'other', 'nevertheless', 'whom', 'for', 'somehow', 
-        'beforehand', 'just', 'an', 'beyond', 'amongst', 'none', "'ve", 'say', 'via', 'but', 'often', 're', 'our', 
-        'because', 'rather', 'using', 'without', 'throughout', 'on', 'she', 'never', 'eight', 'no', 'hereupon', 
-        'them', 'whereafter', 'quite', 'which', 'move', 'thru', 'until', 'afterwards', 'fifty', 'i', 'itself', 'nâ€˜t',
-        'him', 'could', 'front', 'within', 'â€˜re', 'back', 'such', 'already', 'several', 'side', 'whence', 'me', 
-        'same', 'were', 'it', 'every', 'third', 'together'
-    ]}
-    
+def stop_words_remover(df):          '#takes in a pandas dataframe as input'
+"""function to remove english stop words"""
+    new_df = pd.DataFrame([])    '#creat new dataframe'
+    tokenised_list = []          '# tokenised sentence'
     for row in df['Tweets']:
         no_stops = [word for word in row.lower().split() if word not in stop_words_dict['stopwords']]
-        tokenised_list.append(no_stops) # adds words that are not in stop word dictionary into tokenised_list
-    
-    new_df['Tweets'] = df['Tweets'] #creats copy of tweets column
-    new_df['Date'] = df['Date']# creates copy of column Date
-    
-    new_df['without stop words'] = tokenised_list # creates column "Without stop words" containing tokenised list
-    
+        tokenised_list.append(no_stops)   '# adds words not in stop word dictionary into tokenised_list'
+    new_df['Tweets'] = df['Tweets']     '#creats copy of tweets column'
+    new_df['Date'] = df['Date']         '# creates copy of column Date'
+    new_df['Without Stop Words'] = tokenised_list  '# creates column "Without stop words" containing tokenised list'
     return new_df
-### END FUNCTION
     
