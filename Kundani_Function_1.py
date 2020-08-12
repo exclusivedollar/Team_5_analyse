1f8e4b4bd9a1ce8fa60986f1c902bbfe32571c98
Grewies <68637353+Grewies@users.noreply.github.com>
2020-08-12 23:34:25 +0200

    Update Kundani_Function_1.py

--- Kundani_Function_1.py
+++ Kundani_Function_1.py
@@ -1,7 +1,4 @@
 def dictionary_of_metrics(items):
-    
-    # your code here
-    
     """returns a dictionary of the metrics from a list of items """
     
     dict = {'mean':round(np.mean(items),2),
@@ -10,7 +7,5 @@ def dictionary_of_metrics(items):
          'std':round(np.std(items,ddof=1),2),
          'min':round(np.min(items),2),
          'max':round(np.max(items),2)}
-    
-        
     return dict
      
