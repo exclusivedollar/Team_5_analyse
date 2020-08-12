690fc9a5922857cb9ca309b211c22440cfc04b34
Lesedi-Coder <59532191+Lesedi-Coder@users.noreply.github.com>
2020-08-12 03:05:16 +0200

    Update Lesedi_Function_6.py
    Docstring added

--- Lesedi_Function_6.py
+++ Lesedi_Function_6.py
@@ -1,4 +1,11 @@
-def word_splitter(df):    
+def word_splitter(df):   
+    """Splits and lowercases sentences in a dataframe's column into a list of seperate words.
+       Created lists will be placed in a column named 'Split Tweets' in the
+       original dataframe.Also known as tokenization
+       
+       Arguments : Takes in a pandas dataframe as an input
+       Returns: returns a modified dataframe""" 
+       
     dframe = pd.DataFrame(df,columns =['Tweets','Date','Split Tweets'])
     dframe['Date'] = dates
     dframe['Split Tweets']= dframe['Tweets'].str.lower()
