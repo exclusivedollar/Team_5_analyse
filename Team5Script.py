bb8791ef8334bef467476cef5e429d8c1776cc62
Lesedi-Coder <59532191+Lesedi-Coder@users.noreply.github.com>
2020-08-13 00:44:24 +0200

    Update Team5Script.py

--- Team5Script.py
+++ Team5Script.py
@@ -95,12 +95,12 @@ def word_splitter(df):
     
 def stop_words_remover(df):
     """function to remove english stop words"""
-    new_df = pd.DataFrame([])  '#creat new dataframe'
-    tokenised_list = []  '# tokenised sentence'
+    new_df = pd.DataFrame([])  '#creates a new dataframe'
+    tokenised_list = []  '#tokenised sentence'
     for row in df['Tweets']:
         no_stops = [word for word in row.lower().split() if word not in stop_words_dict['stopwords']]
-        tokenised_list.append(no_stops)  '# adds words that are not in stop word dictionary into tokenised_list'
-    new_df['Tweets'] = df['Tweets']  '#creats copy of tweets column'
-    new_df['Date'] = df['Date']  '# creates copy of column Date'
+        tokenised_list.append(no_stops)  '#adds words that are not in stop word dictionary into tokenised_list'
+    new_df['Tweets'] = df['Tweets']  '#creates copy of tweets column'
+    new_df['Date'] = df['Date']  '#creates copy of column Date'
     new_df['Without Stop Words'] = tokenised_list  '# creates column "Without stop words" containing tokenised list'
     return new_df
