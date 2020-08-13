309b8ad6118dd7049f024fc92feb333c3b5247e2
K-Netshiongolwe <68648681+K-Netshiongolwe@users.noreply.github.com>
2020-08-13 12:49:53 +0200

    Update kundani_function.py

--- kundani_function.py
+++ kundani_function.py
@@ -1,4 +1,9 @@
 def dictionary_of_metrics(items):
-
-
-return
+    """returns a dictionary of the metrics from a list of items """
+    dict = {'mean': round(np.mean(items), 2),
+            'median': round(np.median(items), 2),
+            'var': round(np.var(items, ddof=1), 2),
+            'std': round(np.std(items, ddof=1), 2),
+            'min': round(np.min(items), 2),
+            'max': round(np.max(items), 2)}
+    return dict
